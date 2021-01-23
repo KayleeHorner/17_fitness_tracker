785c47453817e526627e27685e35e44cff46c8ef
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-23 01:57:02 -0600

    updated put route to push the object instead of individual components

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -2,60 +2,45 @@ const app = require("express").Router();
 const db = require("../models/exercise");
 
 app.get("/workouts", (_req, res) => {
-  db.find({}).then(data => {
-        res.json(data)
-      })
-      .catch(err => {
-        res.json(err)
-      })
-});
-
-
-app.put("/workouts/:id", ({body, params}, res) => {
-   db.findByIdAndUpdate(params.id, 
-        { 
-            $push: { 
-                workouts: [
-                    {
-                name: body.name,
-                type: body.type,
-                weight: body.weight,
-                sets: body.sets, 
-                reps: body.reps,
-                duration: body.duration, 
-            }
-        ]
-    }})
-    .then(data => {
+  db.find({})
+    .then((data) => {
       res.json(data);
     })
-    .catch(err => {
+    .catch((err) => {
       res.json(err);
     });
-
 });
 
+app.put("/workouts/:id", ({ body, params }, res) => {
+  db.findByIdAndUpdate(params.id, {
+    $push: {
+      exercises: body,
+    },
+  })
+    .then((data) => res.json(data))
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 app.post("/workouts", (req, res) => {
- db.create({})
-     .then(data => {
-       res.json(data);
-     })
-     .catch(err => {
-       res.json(err);
-     });
-
- });
-
-app.get("/workouts/range", (req, res) => {
-  db.find({}, (error, data) => {
-    if (error) {
-      res.json(error);
-    } else {
+  db.create({})
+    .then((data) => {
       res.json(data);
-    }
-  });
+    })
+    .catch((err) => {
+      res.json(err);
+    });
 });
 
+app.get("/workouts/range", (req, res) => {
+  db.find({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
 
 module.exports = app;
