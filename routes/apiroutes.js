696d9b5ec30de80072ed15ca8299da79afc1e506
Kaylee Horner <kayleeahorner@gmail.com>
2021-01-22 19:16:14 -0600

    changed const so schema would pull correctly. need to figure out where saved data is and how to display

--- routes/apiroutes.js
+++ routes/apiroutes.js
@@ -1,18 +1,17 @@
 const app = require("express").Router();
-const mongoose = require("mongoose");
-const db = require("../models");
+const db = require("../models/exercise");
 
-app.get("/api/workouts", (req, res) => {
-    db.find({}, (error, result) => {
-        if (error) {
-          res.send(error);
-        } else {
-          res.send(result);
-        }
-      });
+app.get("/workouts", (_req, res) => {
+  db.find({}).then(data => {
+        res.json(data)
+      })
+      .catch(err => {
+        res.json(err)
+      })
 });
 
-app.put("/api/workouts/:id", ({body, params}, res) => {
+
+app.put("/workouts/:id", ({body, params}, res) => {
    db.findByIdAndUpdate(params.id, 
         { 
             $push: { 
@@ -37,7 +36,7 @@ app.put("/api/workouts/:id", ({body, params}, res) => {
 });
 
 
-app.post("/api/workouts", (req, res) => {
+app.post("/workouts", (req, res) => {
  db.create({})
      .then(data => {
        res.json(data);
@@ -48,110 +47,15 @@ app.post("/api/workouts", (req, res) => {
 
  });
 
-app.get("/api/workouts/range", (req, res) => {
-  db.find({}, (error, result) => {
+app.get("/workouts/range", (req, res) => {
+  db.find({}, (error, data) => {
     if (error) {
-      res.send(error);
+      res.json(error);
     } else {
-      res.send(result);
+      res.json(data);
     }
   });
 });
-
-// app.post("/submit", (req, res) => {
-//   console.log(req.body);
-
-//   db.notes.insert(req.body, (error, data) => {
-//     if (error) {
-//       res.send(error);
-//     } else {
-//       res.send(data);
-//     }
-//   });
-// });
-
-// app.get("/find/:id", (req, res) => {
-//   db.notes.findOne(
-//     {
-//       _id: mongojs.ObjectId(req.params.id)
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
-
-// app.post("/update/:id", (req, res) => {
-//   db.notes.update(
-//     {
-//       _id: mongojs.ObjectId(req.params.id)
-//     },
-//     {
-//       $set: {
-//         title: req.body.title,
-//         note: req.body.note,
-//         modified: Date.now()
-//       }
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
-
-// app.delete("/delete/:id", (req, res) => {
-//   db.notes.remove(
-//     {
-//       _id: mongojs.ObjectID(req.params.id)
-//     },
-//     (error, data) => {
-//       if (error) {
-//         res.send(error);
-//       } else {
-//         res.send(data);
-//       }
-//     }
-//   );
-// });
-
-// app.delete("/clearall", (req, res) => {
-//   db.notes.remove({}, (error, response) => {
-//     if (error) {
-//       res.send(error);
-//     } else {
-//       res.send(response);
-//     }
-//   });
-// });
-
-// db.Library.create({ name: "Campus Library" })
-//   .then(dbLibrary => {
-//     console.log(dbLibrary);
-//   })
-//   .catch(({message}) => {
-//     console.log(message);
-//   });
-
-// app.post("/submit", ({body}, res) => {
-//   db.Book.create(body)
-//     .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-//     .then(dbLibrary => {
-//       res.json(dbLibrary);
-//     })
-//     .catch(err => {
-//       res.json(err);
-//     });
-// });
-
-
 
 
 module.exports = app;
